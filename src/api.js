61862a1cd131c445d02327390a998a3a96e5e8a8
Harmandeep Singh <mr.harmandeeps@gmail.com>
2022-07-08 01:16:00 +0530

    initial commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'bde60ed8c4mshead11abaf4173a4p13a5ecjsnc9670a4ce324',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL="https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "ca4ff61631a151de2d1b93b6ed314a96"; // enter your key from openweather API
